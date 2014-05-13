8e57b7920d964ace3430130af86e2a53a54ed34c
dimitar9 <dimitar9909@gmail.com>
2014-05-12 23:17:08 -0400

    version 2

--- combination_sum.cpp
+++ combination_sum.cpp
@@ -59,3 +59,44 @@ public:
         return res;
     }
 };
+
+
+
+
+///version 2. no duplicattion from source
+
+
+class Solution {
+public:
+    void dfs(vector<int> &num, int target, vector<vector<int> > &res, vector<int> &r,int st){
+        if (target<0){
+            return;
+        }else{
+            if (target==0){
+                res.push_back(r);
+            }else{
+                int pre = -1;
+                for (int i=st;i<num.size();i++){
+                    if (num[i]!=pre){
+                        r.push_back(num[i]);
+                        dfs(num,target-num[i],res,r,i+1);
+                        pre = num[i];
+                        r.pop_back();
+                    }
+                }
+            }
+        }
+    }
+     
+    vector<vector<int> > combinationSum2(vector<int> &num, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<vector<int> > res;
+        if (num.size()==0){return res;}
+        sort(num.begin(),num.end());
+        vector<int> r;
+        dfs(num,target,res,r,0);
+        return res;
+    }
+};
+

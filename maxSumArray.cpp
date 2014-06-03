355287463fee2288f117903b5c11abaeb842bc8e
Dimitar9 <dimitar9909@gmail.com>
2014-06-02 22:04:59 -0400

    Create maxSumArray.cpp

---
+++ maxSumArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(int A[], int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int m=INT_MIN;
+        int sum=0;
+        for (int i=0;i<n;i++){
+            sum = sum>=0?(sum+A[i]):A[i];
+            m=max(sum,m);
+        }
+        return m;
+    }
+};

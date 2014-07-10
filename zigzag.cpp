45a9a1c6d18355859f836ffcf6995839ff377cd9
Dimitar9 <dimitar9909@gmail.com>
2014-07-09 22:11:02 -0400

    Update zigzag.cpp

--- zigzag.cpp
+++ zigzag.cpp
@@ -1,28 +1,52 @@
-string convert(string s, int nRows) {
-    int len = s.length();
-    if (len < 3 || nRows == 1)
-        return s;
-
-    bool down = true;
-    vector<vector<int> > tab(nRows);
-    for (int i = 0, row = 0; i < len;) {
-        if (down && row < nRows) 
-            tab[row++].push_back(s[i++]);
-        else if (!down && row >= 0)
-            tab[row--].push_back(s[i++]);
-
-        if (row < 0 || row >= nRows) {
-            row += (down ? -2 : 2);
-            down = !down;
+class Solution {
+public:
+    string convert(string s, int nRows) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function    
+        if(nRows <= 1) return s;
+        string ret;
+        int zigsize = 2 * nRows - 2;
+        for(int i = 0; i < nRows; ++i) {
+            for(int base = i; ;base += zigsize) {
+                if(base >= s.size())
+                    break;
+                ret.append(1,s[base]);
+                if(i > 0 && i < nRows - 1) {
+                    //middle need add ziggggging char
+                    int ti = base + zigsize - 2 * i;
+                    if(ti < s.size())
+                        ret.append(1,s[ti]);
+                }
+            }
         }
+        return ret;
     }
+};
 
-    string ret = "";
-    vector<vector<int> >::iterator it = tab.begin();
-    for (; it != tab.end(); ++it) {
-        vector<int>::iterator it2 = (*it).begin();
-        for (; it2 != (*it).end(); ++it2)
-            ret += *it2;
-    }
-    return ret;
-}
+
+/*
+直观的算法，写一下不同行数下的例子就能找到规律了。
+
+ nRows = 2
+0 2 4 6 ...
+1 3 5 7
+
+ nRows = 3
+0   4   8  ...
+1 3 5 7 9
+2   6   10
+
+ nRows = 4
+0     6       12 ...
+1   5 7    11
+2 4   8 10  
+3     9
+
+先计算一下每一个zig包含的字符个数，实际上是zigSize = nRows + nRows – 2
+然后一行一行的加s中的特定元素就行。
+第一行和最后一行都只需要加一个字符，每一个zig，而且在s中的index间隔是zigSize。
+中间每一行需要添加两个字符到结果中去。第一个字符同上；第二个字符和前面添加这个字符在s上的inde相差正好是zigSize – 2*ir。ir是行index。
+
+
+
+*/

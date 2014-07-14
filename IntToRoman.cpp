77633ed41375b19f63ebc514d19c1507ffc96638
Dimitar9 <dimitar9909@gmail.com>
2014-07-14 00:13:57 -0400

    Update IntToRoman.cpp

--- IntToRoman.cpp
+++ IntToRoman.cpp
@@ -1,3 +1,17 @@
+
+/*
+
+    A number written in Arabic numerals can be broken into digits. For example, 1903 is composed of 1 (one thousand), 9 (nine hundreds), 0 (zero tens), and 3 (three units). To write the Roman numeral, each of the non-zero digits should be treated separately. In the above example, 1,000 = M, 900 = CM, and 3 = III. Therefore, 1903 = MCMIII.[4]
+    
+    The symbols "I", "X", "C", and "M" can be repeated three times in succession, but no more. (They may appear more than three times if they appear non-sequentially, such as XXXIX.) "D", "L", and "V" can never be repeated.[5][6]
+    
+    "I" can be subtracted from "V" and "X" only. "X" can be subtracted from "L" and "C" only. "C" can be subtracted from "D" and "M" only. "V", "L", and "D" can never be subtracted[6]
+    
+    Only one small-value symbol may be subtracted from any large-value symbol.[7]
+
+
+*/
+
 class Solution {
 public:
     string intToRoman(int num) {

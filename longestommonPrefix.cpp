ef03f7bf1e270b0cfd3cbd46c24461830d741bf3
Dimitar9 <dimitar9909@gmail.com>
2014-07-14 23:01:19 -0400

    Update longestommonPrefix.cpp

--- longestommonPrefix.cpp
+++ longestommonPrefix.cpp
@@ -16,3 +16,26 @@ public:
         return str.substr(0,i);
     }
 };
+
+my version:
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        if (strs.size() < 1) return "";
+        if (strs.size() == 1) return strs[0];
+        int i =0; int j = 0;bool b_flag = false;
+       
+        for(i=0; (i<strs[0].length()) && (b_flag == false);i++){
+            char a = strs[0][i];
+            for(j=1;j<strs.size();j++){
+                if(strs[j][i] != a){
+                    b_flag = true; //last run was a mismatch. need to minus i.
+                    break;
+                }
+            }
+        }
+        if (b_flag) i--;//last run was a mismatch. need to minus i.
+        return strs[0].substr(0,i);
+    }
+};

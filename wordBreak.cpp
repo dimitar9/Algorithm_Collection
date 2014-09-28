9d739b0374f5abad6cef587ff7ea655923fe5e76
Dimitar9 <dimitar9909@gmail.com>
2014-09-28 17:45:34 -0400

    Create wordBreak.cpp

---
+++ wordBreak.cpp
@@ -0,0 +1,30 @@
+//http://www.programcreek.com/2012/12/leetcode-solution-word-break/
+
+public class Solution {
+    public boolean wordBreak(String s, Set<String> dict) {
+        boolean[] t = new boolean[s.length()+1];
+        t[0] = true; //set first to be true, why?
+        //Because we need initial state
+ 
+        for(int i=0; i<s.length(); i++){
+            //should continue from match position
+            if(!t[i]) 
+                continue;
+ 
+            for(String a: dict){
+                int len = a.length();
+                int end = i + len;
+                if(end > s.length())
+                    continue;
+ 
+                if(t[end]) continue;
+ 
+                if(s.substring(i, end).equals(a)){
+                    t[end] = true;
+                }
+            }
+        }
+ 
+        return t[s.length()];
+    }
+}

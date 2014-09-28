33d1e35bd113cd98694e8f8aefee5c8ebd611be1
Dimitar9 <dimitar9909@gmail.com>
2014-09-28 18:14:49 -0400

    Update reverseWords.cpp

--- reverseWords.cpp
+++ reverseWords.cpp
@@ -22,3 +22,26 @@ public:
         s = s.substr(0,s.size()-1); //eliminate the last space
     }
 };
+
+
+
+
+class Solution {
+public:
+    void reverseWords(string &s) {//reverse every single word then reverse the whole string
+        string rs;
+        
+        for(int i = s.length()-1; i>=0;){
+            while(i>=0 && s[i] == ' ') i--;
+            if(i<0) break;
+            if(!rs.empty()) rs.push_back(' ');
+            string tmp;
+            while(i>=0 && s[i] != ' ') {tmp.push_back(s[i--]);}
+            reverse(tmp.begin(), tmp.end());
+            rs.append(tmp);
+            
+        }
+        
+        s = rs;
+    }
+};

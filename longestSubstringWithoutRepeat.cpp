d51a7b63c55acfd7fd9ddd4676a547ca726b0a4c
dimitar9 <dimitar9909@gmail.com>
2014-10-02 01:33:43 -0400

    longest substr

--- longestSubstringWithoutRepeat.cpp
+++ longestSubstringWithoutRepeat.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxLen=0;
-        for(int i=0;i<s.length();i++)
-        {
-                std::map<int,int> currmap;
-                currmap[s[i]]=0;
-                if (s.length() ==1) return 1; //this is important. easy to miss.
-                for(int j=i+1;j<s.length();j++)
-                {
-                    if  (currmap.find(s[j]) != currmap.end())
-                    {
-                        int len = currmap.size();
-                        maxLen = max(maxLen,len);
-                        break;
-                    }
-                    else
-                    {
-                        currmap[s[j]]=0;
-                    }
-                    if (j==s.length()-1) //this is important. easy to miss.
-                    {
-                        int len = currmap.size();
-                        maxLen = max(maxLen,len);
-                        break;
-                    }
-                }
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(s.size()==0){return 0;}
+        if(s.size()==1){return 1;}
+        int i=0;
+        int j=0;
+        int maxl = 0;
+        map<char,bool> table;
+        while ( (i<s.size()) && (j<s.size()) ){
+            if (table[s[j]]==false){
+                table[s[j]]=true;
+                maxl = max(maxl,j-i+1);
+                j++;
+            }else if (table[s[j]]==true){
+                maxl = max(maxl,j-i);
+                table[s[i]]=false;
+                i++;
+            }
         }
-        return maxLen;
-        
+        return maxl;
     }
 };

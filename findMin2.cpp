625e17b38203b08d5647fdce322ba731385869d2
Dimitar9 <dimitar9909@gmail.com>
2014-10-21 11:58:11 -0400

    Create findMin2.cpp

---
+++ findMin2.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+	int helper(vector<int> &num, int st, int end) {
+		if (st == end)
+			return num[st];
+		if (end - st == 1)
+			return (num[st] > num[end]) ? num[end] : num[st];
+		int mid = st + (end - st + 1) / 2;
+	//	if ((num[mid] == num[st]) && (num[mid] == num[end]))
+	//		return num[st];
+		if (num[mid] > num[end]) {
+			if (num[end] <= num[st])
+				return helper(num, mid, end);
+			else
+				return helper(num, st, mid);
+		}
+
+        else if (num[mid] < num[end]) {
+            if (mid < 1)
+                return helper(num, mid, end);
+            if (num[mid] < num[mid - 1])
+                return helper(num, mid, end);
+            if (num[mid] > num[mid - 1])
+                return helper(num, st, mid - 1);
+            else {
+                int ori_mid = mid;
+                mid--;
+                while (mid >= 0)  {
+                    if (num[mid] < num[mid + 1])
+                        return helper(num, st, ori_mid);
+                    else if (num[mid] > num[mid+1])
+                        return helper(num, ori_mid, end);
+                    mid--;
+                }
+                return helper(num, ori_mid, end);
+            }
+        } else if (num[mid] == num[end]) {
+            return min(helper(num, st, mid),helper(num,mid+1,end));
+        }
+
+
+	}
+
+	int findMin(vector<int> &num) {
+		int len = num.size();
+		if (len == 1)
+			return num[0];
+		return helper(num, 0, len - 1);
+
+	}
+
+};

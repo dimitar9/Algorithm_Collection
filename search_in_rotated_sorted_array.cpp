75825114b5fc4f14bad07e9867647161ee8f00ea
Dimitar9 <dimitar9909@gmail.com>
2014-10-21 22:53:24 -0400

    Update search_in_rotated_sorted_array.cpp

--- search_in_rotated_sorted_array.cpp
+++ search_in_rotated_sorted_array.cpp
@@ -1,55 +1,49 @@
-/*leetcode Question 94: Search in Rotated Sorted Array 
- Search in Rotated Sorted Array
-
- Suppose a sorted array is rotated at some pivot unknown to you beforehand.
- (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
- You are given a target value to search. If found in the array return its index, otherwise return -1.
- You may assume no duplicate exists in the array.
- Main idea is Binary Search.
- As the sequence is rotated, for any mid element, either it is of order with its previous part, or it is of order with its next part. e.g. 561234, middle element 1 has an order with its next part 1234.
-                                  5678123, middle element 8 has an order with its previous part 5678.
-
- Normal binary search just compare the middle element with the target, here we need more than that.
- e.g.
- 567123, the middle element is 1, if we want to search 6, first compare middle element with 1st element, to know which part is of order, if mid>1st, the first part is ordered,otherwise the last part is ordered. Then compare the target value with the bound value in each case. Details see the code.
-
-
- Complexity is O(log_n).*/
 class Solution {
 public:
- 
-    int se(int st, int ed, int target, int A[]){
-        if (st>ed) {return -1;}
-        else{
-            int mid = st+(ed-st)/2;
-            if (A[mid]==target){return mid;}
-             
-            if (A[mid]>=A[st]){
-                if (target<=A[mid] && target>=A[st]){
-                    return se(st,mid-1,target,A);
-                }else{
-                    return se(mid+1,ed,target,A);
-                }
-            }
-             
-            if (A[mid]<A[st]){
-                 
-                if (target<=A[mid] || target >= A[st]){
-                    return se(st,mid-1,target,A);
-                }else{
-                    return se(mid+1,ed,target,A);
-                }
-                 
-            }
-            return -1;
+     // two branches, 6 if statement!
+     // don't forget to check n==1 n==0 2 cases
+    int helper(int A[], int st, int end, int target,int n) {
+        if ((n==0) || (st>end)) return -1;
+        if (n==1){
+            return (A[st]==target)?st:-1;
         }
-         
+        int mid = st+ (end - st + 1)/2;
+        
+        if (A[mid]==target) {
+            return mid;
+        }
+        if (A[mid] >= A[st]) {
+            if(target < A[mid]  && (target >= A[st])) {
+                return helper(A,st,mid-1,target,mid-st);
+            }
+            if(target < A[mid]  && (target <= A[st])) {
+                return helper(A,mid+1,end,target,end-mid);
+            }
+            if(target > A[mid]  ) { //very tricky this line
+                return helper(A,mid+1,end,target,end-mid);
+            }
+           
+        }
+        if (A[mid] < A[st]) {
+            if(target > A[mid]  && (target >= A[st])) {
+                return helper(A,st,mid-1,target,mid-st);
+            }
+            if(target > A[mid]  && (target <= A[st])) {
+                return helper(A,mid+1,end,target,end-mid);
+            }
+            if(target < A[mid] ) { // very trick this line
+                return helper(A,st,mid-1,target,mid-st);
+            }
+        }
+
+        
     }
- 
     int search(int A[], int n, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (n==0){return -1;}
-        return  se(0,n-1,target,A);
+        if (n==0) return -1;
+        if (n==1){
+            return (A[0]==target)?0:-1;
+        }
+        else return helper(A,0,n-1,target,n);
+        
     }
 };

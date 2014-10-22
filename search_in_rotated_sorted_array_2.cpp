af584aa24b13233587a951e9bb36fc2e7984b272
Dimitar9 <dimitar9909@gmail.com>
2014-10-21 23:30:44 -0400

    Create search_in_rotated_sorted_array_2.cpp

---
+++ search_in_rotated_sorted_array_2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+ 
+    bool se(int st, int ed, int target, int A[]){
+        if (st>ed) {return false;}
+        else{
+            int mid = st+(ed-st)/2;
+            if (A[mid]==target){return true;}
+             
+            if (A[mid]>A[st]){
+                if (target<=A[mid] && target>=A[st]){
+                    return se(st,mid-1,target,A);
+                }else{
+                    return se(mid+1,ed,target,A);
+                }
+            }
+             
+            if (A[mid]<A[st]){
+                 
+                if (target<=A[mid] || target >= A[st]){
+                    return se(st,mid-1,target,A);
+                }else{
+                    return se(mid+1,ed,target,A);
+                }
+                 
+            }
+             
+            if (A[mid]==A[st]){return se(st+1,ed,target,A);}
+             
+            return false;
+       } 
+         
+    }
+ 
+    bool search(int A[], int n, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (n==0){return false;}
+        return  se(0,n-1,target,A);
+    }
+};

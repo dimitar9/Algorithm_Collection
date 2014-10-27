4ef1b15632f234f665493875f86fea70d0c1bb36
Dimitar9 <dimitar9909@gmail.com>
2014-10-26 22:47:17 -0400

    Update generate_parenthesis.cpp

--- generate_parenthesis.cpp
+++ generate_parenthesis.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        string candy;
-        if ( n!=0)
-            generate_p(result, candy, n);
-        return result;
+
+    void gp(int l, int r, int n, string str, vector<string>& res){
+        if (l>n) return;
+        if ( r==n ) res.push_back(str);
+        else{
+            gp(l+1,r,n,str+'(',res);
+            if(l>r){
+                gp(l,r+1,n,str+')',res);
+            }
+        }
     }
-    void generate_p(vector<string>& result, string& candy, int n){
-        int leftp = 0; 
-        int rightp = 0;
-        int curr_len = candy.length();
-        if (curr_len == n*2)
-        {
-            result.push_back(candy);
-        }
-        for(int i =0; i< curr_len; i++) {
-            if (candy[i] == '(')
-                leftp++;
-            else
-                rightp++;
-        }
-        if (leftp < n) {
-            //*candy.append("(");
-            candy.push_back('(');
-            generate_p(result, candy, n);
-            candy= candy.substr(0,candy.length()-1);
-        }
-         if ( (leftp > rightp) )
-        {
-            candy.push_back(')');
-            generate_p(result,candy,n);
-            candy= candy.substr(0,candy.length() - 1);
-        }
-        if(leftp > n) 
-            return;
-   } 
+        
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        if(n==0) return res;
+        
+        gp(0,0,n,"",res);
+        
+    }
 };

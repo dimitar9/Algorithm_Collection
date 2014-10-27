e9ab1a3bb1c56eaf628e7daddd409bd8e5e1f679
Dimitar9 <dimitar9909@gmail.com>
2014-10-26 21:41:59 -0400

    Update and rename uniquePaths.cpp to unique_paths.cpp

--- unique_paths.cpp
+++ unique_paths.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int **arr = new int* [m];
-        for (int i=0;i<m;i++){
-            arr[i]= new int[n];
+int uniquePaths(int m, int n) {
+        if ((m <= 0)  || (n <= 0)) return 0;
+        vector<vector<int> > res(m, vector<int> (n));
+        for(int i = 0; i < m; i ++ ){
+            res[i][0] = 1;
         }
-        arr[0][0]=1;
-        for (int i=0;i<m;i++){
-            arr[i][0] = 1;
-        }
-        for (int i=0;i<n;i++){
-            arr[0][i] = 1;
-        }
-        for (int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                arr[i][j] = arr[i][j-1] + arr[i-1][j];
+        for(int j = 0; j < n; j ++ ){
+            res[0][j] = 1;
+        }       
+        for (int i=1; i<m; i++ ) {
+            for (int j=1; j<n; j++) {
+               res[i][j] = res[i-1][j] + res[i][j-1];
             }
-        }  
-        return arr[m-1][n-1];
-    }
+        }
+        return res[m-1][n-1];
+        
+}
+
+
+    
+};
 
 
 

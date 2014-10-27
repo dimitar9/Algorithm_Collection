d56d7307fc6d1cdb3d93eac7014a22257501ebc5
Dimitar9 <dimitar9909@gmail.com>
2014-10-26 23:34:26 -0400

    Update rotate_image.cpp

--- rotate_image.cpp
+++ rotate_image.cpp
@@ -10,18 +10,22 @@ better solution:
 class Solution {
 public:
     void rotate(vector<vector<int> > &matrix) {
+
         int n = matrix.size();
-        //转置
-        for(int i = 0; i < n; i++)
-            for(int j = i+1; j < n; j++)
-                swap(matrix[i][j] , matrix[j][i]);
-        //每一行翻转
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < (n>>1); j++)
-                swap(matrix[i][j], matrix[i][n-j-1]);
+        for (int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+            
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                swap(matrix[i][j],matrix[i][n-j-1]);
+            }
+        }
+         
     }
 };
-
 
 
 

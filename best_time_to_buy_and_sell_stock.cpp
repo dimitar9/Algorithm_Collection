932b953e710c95d3ee4c6bd8b0a887eaff8d2371
Dimitar9 <dimitar9909@gmail.com>
2014-10-27 00:20:46 -0400

    Update best_time_to_buy_and_sell_stock.cpp

--- best_time_to_buy_and_sell_stock.cpp
+++ best_time_to_buy_and_sell_stock.cpp
@@ -4,7 +4,7 @@ public:
         if (prices.size()<=1) return 0;
         int min_val = INT_MAX; 
         int max_profit = 0;
-        for(int i=0;i<prices.size();i++){/
+        for(int i=0;i<prices.size();i++){
             max_profit  = max(max_profit,prices[i]-min_val);
             min_val = min(min_val,prices[i] );
         }

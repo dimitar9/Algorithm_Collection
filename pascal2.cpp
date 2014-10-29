59e8b2de212385df327ebc55b78ff45741fb8a04
Dimitar9 <dimitar9909@gmail.com>
2014-10-28 22:30:18 -0400

    Update pascal2.cpp

--- pascal2.cpp
+++ pascal2.cpp
@@ -1,23 +1,15 @@
-[解题报告]
-滚动数组实现。注意Line11，要从后往前加，否则会产生冗余计算。
 
-[Code]
-
-1:    vector<int> getRow(int rowIndex) {  
-2:      // Start typing your C/C++ solution below  
-3:      // DO NOT write int main() function  
-4:      vector<int> result;  
-5:      result.resize(rowIndex+2);  
-6:      for(int i =0; i< rowIndex+2; i++)  
-7:        result[i] = 0;  
-8:      result[1]=1;  
-9:      for(int i =0; i< rowIndex; i++)  
-10:      {  
-11:        for(int j =rowIndex+1; j>0; j--)  
-12:        {  
-13:          result[j] = result[j-1] + result[j];  
-14:        }  
-15:      }  
-16:      result.erase(result.begin());  
-17:      return result;  
-18:    }  
+class Solution {
+public:
+  vector<int> getRow(int rowIndex) {
+  vector<int> ret(rowIndex + 1);
+  ret[0] = 1;
+  for(int i = 1; i <= rowIndex; ++i){
+    for(int j = i; j > 0; --j){
+      if(j == i) ret[j] = 1;
+      else ret[j] = ret[j - 1] + ret[j];
+    }
+  }
+  return ret;
+  }
+};

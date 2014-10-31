f7a7ed629e4a1c693bd6bbedffd035899cfc5de2
Dimitar9 <dimitar9909@gmail.com>
2014-10-31 01:46:10 -0400

    Update search_for_a_range.cpp

--- search_for_a_range.cpp
+++ search_for_a_range.cpp
@@ -1,51 +1,42 @@
-/*leetcode Question 93: Search for a Range 
- Search for a Range
-
- Given a sorted array of integers, find the starting and ending position of a given target value.
- Your algorithm's runtime complexity must be in the order of O(log n).
- If the target is not found in the array, return [-1, -1].
- For example,
- Given [5, 7, 7, 8, 8, 10] and target value 8,
- return [3, 4].
- Updated 201309
- Analysis:
- When see the O(log n) complexity requirement, it is highly possible that binary search is needed.
- As the array is sorted, thus, the "next" element of the result range must be less (left) or greater
- (right) than the target value, or meets the boundary of the array. So, we can do the binary search 
- twice, one search the left range, where A[mid-1]<A[mid], and one search for the right range, where A[mid+1]>A[mid].*/
+//dmitar slution
 class Solution {
 public:
-    int search(int A[], int n, int target,int st,int ed, bool left){
-        if (st>ed){
-            return -1;
-        }else{
-            int mid = st+(ed-st)/2;
-            if (A[mid]==target){
-                if (left){
-                    if (mid==0 || A[mid-1]<A[mid]){return mid;}
-                    else{return search(A,n,target,st,mid-1,left);}
+    vector<int> searchRange(int A[], int n, int target) {
+        vector<int> a(2,-1);
+        int st=-1; 
+        int end = -1;
+
+        
+        if( n <1) return a;
+        if(n==1) if (A[0]==target) {
+            a[0] = 0;
+            a[1] = 0;
+            return a;
+        }
+        
+        st = 0;
+        end = n-1;
+        int mid = 0;
+        while(st<=end){
+            mid = st + (end-st)/2;
+            if(A[mid] < target){
+                st = mid +1;
+            } else if (A[mid] > target){
+                end = mid - 1;
+            } else if(A[mid] == target){
+                st = mid;
+                while( st > 0 && A[mid] == A[st-1]){
+                    st--;
                 }
-                if (!left){
-                    if (mid==n-1 || A[mid+1]>A[mid]){return mid;}
-                    else{return search(A,n,target,mid+1,ed,left);} 
+                end = mid;
+                while(end < n-1 && A[mid] == A[end+1]){
+                    end++;
                 }
-            }
-            if (A[mid]<target){
-                return search(A,n,target,mid+1,ed,left);
-            }
-            if (A[mid]>target){
-                return search(A,n,target,st,mid-1,left);
+                a[0]=st;a[1]=end;
+                return a;
             }
         }
-    }
-     
-    vector<int> searchRange(int A[], int n, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        vector<int> res(2,-1);
-        res[0]=search(A,n,target,0,n-1,true);
-        res[1]=search(A,n,target,0,n-1,false);
-        return res;        
+        
+        
     }
 };
-    

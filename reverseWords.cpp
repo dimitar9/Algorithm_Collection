a3f49e99d12509c5f743da219a717641b052bbbf
Dimitar9 <dimitar9909@gmail.com>
2014-11-24 01:39:20 -0500

    Update reverseWords.cpp

--- reverseWords.cpp
+++ reverseWords.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
-    void reverseWords(string &s) {
-        string word; //tmp string to store each word
-        string res; // result string
-        int i=0;
-        while (i<s.size()){
-            if (char(s[i])==' ' && word.empty()){i++;continue;} //multiple spaces
-            if (char(s[i])==' ' && !word.empty()){ //first space after a word
-                res = word+" "+ res; //store the word
-                word=""; //reset the word
-                i++; continue;
+        void reverseWords(string &s) {
+        string reversed;
+        int j = s.size();
+        for(int i = s.size()-1; i >=0; i--){
+            if (s[i] == ' ') {
+                j = i;
+            } else if (i==0 || s[i-1] == ' ') {
+                if (reversed.size() !=0) {
+                    reversed += ' ';
+                }
+                reversed += (s.substr(i, j-i));
             }
-            if (char(s[i])!=' '){word=word+char(s[i]);i++; continue;} //non-space chars
-        }
-         
-        if (!word.empty()){ //last word
-            s = word+" "+res;
-        }else{
-            s = res;
-        }
-        s = s.substr(0,s.size()-1); //eliminate the last space
-    }
-};
-
-
-
-
-class Solution {
-public:
-    void reverseWords(string &s) {//reverse every single word then reverse the whole string
-        string rs;
-        
-        for(int i = s.length()-1; i>=0;){
-            while(i>=0 && s[i] == ' ') i--;
-            if(i<0) break;
-            if(!rs.empty()) rs.push_back(' ');
-            string tmp;
-            while(i>=0 && s[i] != ' ') {tmp.push_back(s[i--]);}
-            reverse(tmp.begin(), tmp.end());
-            rs.append(tmp);
             
         }
-        
-        s = rs;
+        s = reversed;
+    
     }
 };

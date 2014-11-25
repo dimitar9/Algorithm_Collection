21c7568c8da46c45ff136a0913173df1255edf85
dimitar9 <dimitar9909@gmail.com>
2014-11-25 00:16:08 -0500

    a

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -16,3 +16,34 @@ public:
         return ret;
     }
 };
+
+
+//java version:
+public class Solution {
+    public int reverse(int x) {
+        int ret = 0;
+        while (x != 0) {
+            if (Math.abs(ret) > Integer.MAX_VALUE/10) {
+                return 0;
+            }
+            ret = ret * 10 + x % 10;
+            x = x/10;
+        }
+        return ret;
+    }
+};
+
+//python version
+
+class Solution:
+    # @return an integer
+    def reverse(self, x):
+        answer = 0
+        sign = 1 if x > 0 else -1
+        x = abs(x)
+        while x > 0:
+            if answer > 214748364:
+                return 0
+            answer = answer * 10 + x % 10
+            x /= 10
+        return sign*answer

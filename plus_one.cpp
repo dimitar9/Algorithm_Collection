c6c26c981c3b6623b7f1cec9483854f0f49d8c26
dimitar9 <dimitar9909@gmail.com>
2014-11-26 00:47:39 -0500

    optimize plus one

--- plus_one.cpp
+++ plus_one.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
-        int n = digits.size();
-        int needOneMore = 1;
-        for(int i=0;i<n;i++){
-            if(digits[i]!=9){
-                needOneMore = 0;
-                break;
-            }
-        }
-        vector<int> res(n+needOneMore,0);
-       
-        int carry = 1;
         
-        for(int i=n-1;i>=0;i-- ){
-            if( (carry + digits[i]) <=9){
-                res[i+needOneMore] = carry + digits[i];
-                carry=0;
-                //return digits;
+        for(int i=digits.size()-1;i>=0;i-- ){
+            if( ( digits[i]<9) ){
+                digits[i]++;
+                return digits;
             }
             else {
-                res[i+needOneMore] = carry + digits[i]-10;
-                carry = 1;
+                digits[i]=0;
             }
         }
-        if (needOneMore){
-            res[0]=1;
-        }
-        return res;
+        digits.push_back(0);
+        digits[0]=1;
+        
+        return digits;
         
     }
 };

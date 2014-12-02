67dbd51ba7890ff112d7e31cc0b64ddcd0a82a5a
Dimitar9 <dimitar9909@gmail.com>
2014-12-01 21:49:25 -0500

    Update nqueens.cpp

--- nqueens.cpp
+++ nqueens.cpp
@@ -13,51 +13,46 @@ There exist two distinct solutions to the 4-queens puzzle:
   "Q...",
   "...Q",
   ".Q.."]
-]
+]  
+此题和数独问题类似，每试一个位子，去检查合法性，合法性包括行，列，对角线。 A[i]表示第i行 第A[i]列放置Q。 
 */
 class Solution {
 public:
  
-    vector<vector<string> > res;
-     
-    void printres(vector<int> A,int n){
-        vector<string> r;
-        for(int i=0;i<n;i++){
-            string str(n,'.');
-            str[A[i]]='Q';
-            r.push_back(str);
-        }
-        res.push_back(r);
-    }
-     
-     
-    bool isValid(vector<int> A, int r){
-        for (int i=0;i<r;i++){
-            if ( (A[i]==A[r])||(abs(A[i]-A[r])==(r-i))){
-                return false;
-            }
-        }
-        return true;
-    }
-     
-    void nqueens(vector<int> A, int cur, int n){
-        if (cur==n){printres(A,n);}
-        else{
-            for (int i=0;i<n;i++){
-                A[cur]=i;
-                if (isValid(A,cur)){
-                    nqueens(A,cur+1,n);
-                }
-            }
-        }
-    }
-      
-    vector<vector<string> > solveNQueens(int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        res.clear();
-        vector<int> A(n,-1);
-        nqueens(A,0,n);
-        return res;
-    }
+  bool isValid(vector<int> A, int i){
+      for(int r=0;r<i;r++)
+          if(A[r]==A[i]||abs(A[r]-A[i])==abs(r-i))
+              return false;
+      return true;
+  }
+  void foo(vector<vector<string>> &res, vector<int> &A, int cur, int n){
+      if(cur==n){
+          vector<string> r;
+          for(int i=0;i<n;i++){
+              string str(n,'.');
+              str[A[i]]='Q';
+              r.push_back(str);
+          }
+          res.push_back(r);  
+          return;
+      }
+       
+      for(int i=0;i<n;i++){
+          A[cur]=i;
+          if(isValid(A,cur)){
+               
+              foo(res,A,cur+1,n);
+          }
+      }
+       
+  }
+   
+  vector<vector<string> > solveNQueens(int n) {
+      // Start typing your C/C++ solution below
+      // DO NOT write int main() function
+      vector<vector<string> > res;
+      vector<int> A(n,-1);
+      foo(res, A, 0, n);
+      return res;
+  }
 };

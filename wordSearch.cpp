3b6e071f7a4128e4e8ba844c1011781b36bfd26f
Dimitar9 <dimitar9909@gmail.com>
2014-12-05 23:59:30 -0500

    Update wordSearch.cpp

--- wordSearch.cpp
+++ wordSearch.cpp
@@ -1,62 +1,46 @@
-class Solution {
-public:
- 
-    bool search(vector<vector<char> > &board, int i,int j,string str,vector<vector<bool> > &mask){
-        if (str.size()==0){return true;}
-        else{
-            if ((i>0)&&(board[i-1][j]==str[0])&&(mask[i-1][j]==false)){
-                mask[i-1][j]=true;
-                if (search(board,i-1,j,str.substr(1),mask)){
-                    return true;
-                }
-                mask[i-1][j]=false;
-            }
-            if ((i<board.size()-1)&&(board[i+1][j]==str[0])&&(mask[i+1][j]==false)){
-                mask[i+1][j]=true;
-                if (search(board,i+1,j,str.substr(1),mask)){
-                    return true;
-                }
-                mask[i+1][j]=false;
-            }
-            if ((j>0)&&(board[i][j-1]==str[0])&&(mask[i][j-1]==false)){
-                mask[i][j-1]=true;
-                if (search(board,i,j-1,str.substr(1),mask)){
-                    return true;
-                }
-                mask[i][j-1]=false;
-            }
-            if ((j<board[0].size()-1)&&(board[i][j+1]==str[0])&&(mask[i][j+1]==false)){
-                mask[i][j+1]=true;
-                if (search(board,i,j+1,str.substr(1),mask)){
-                    return true;
-                }
-                mask[i][j+1]=false;
-            }
+bool foo(vector<vector<char> > &board, int i, int j, string word){
+        if (word.size()==0) return true;
+         
+        if(i>0&&board[i-1][j]==word[0]){
+            char ch= board[i][j];
+            board[i][j]='#';
+            if(foo(board,i-1,j,word.substr(1,word.size())))
+                return true;
+            board[i][j]=ch;
         }
+        if(i<board.size()-1&&board[i+1][j]==word[0]){
+            char ch= board[i][j];
+            board[i][j]='#';
+            if(foo(board,i+1,j,word.substr(1,word.size())))
+                return true;
+            board[i][j]=ch;
+        }
+        if(j>0&&board[i][j-1]==word[0]){
+            char ch= board[i][j];
+            board[i][j]='#';
+            if(foo(board,i,j-1,word.substr(1,word.size())))
+                return true;
+            board[i][j]=ch;
+        }
+        if(j<board[0].size()-1&&board[i][j+1]==word[0]){
+            char ch= board[i][j];
+            board[i][j]='#';
+            if(foo(board,i,j+1,word.substr(1,word.size())))
+                return true;
+            board[i][j]=ch;
+        }
+         
         return false;
+             
     }
- 
     bool exist(vector<vector<char> > &board, string word) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (word.size()==0) {return false;}
-             
-        for (int i=0;i<board.size();i++){
-            for (int j=0;j<board[0].size();j++){
-                if (word[0]==board[i][j]){
-                    if (word.size()==1) {return true;}
-                    else{
-                        vector<vector<bool> > mask(board.size(),vector<bool>(board[0].size(),false));
-                        mask[i][j]=true;
-                        if (search(board,i,j,word.substr(1),mask)){
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
+        for(int i=0;i<board.size();i++)
+           for(int j=0;j<board[0].size();j++){
+                if(board[i][j]==word[0])
+                    if(foo(board, i,j,word.substr(1,word.size())))
+                        return true;
+           }
         return false;
     }
-};
-
-reviewed

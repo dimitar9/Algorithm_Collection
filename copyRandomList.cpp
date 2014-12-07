fa40c99a7fc98854bf817b03ff787e96877e1d7d
Dimitar9 <dimitar9909@gmail.com>
2014-12-06 22:30:05 -0500

    Update copyRandomList.cpp

--- copyRandomList.cpp
+++ copyRandomList.cpp
@@ -1,35 +1,50 @@
-struct RandomListNode{
-	int label;
-	RandomListNode *next, *random;
-	RandomListNode(int x): label(x), next(NULL),random(NULL){}
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+     RandomListNode *copyRandomList(RandomListNode *head) {
+         //insert nodes
+         RandomListNode * cur = head;
+         while(cur!=NULL)
+         {
+             RandomListNode* temp = new RandomListNode(cur->label);
+             temp->next = cur->next;
+             cur->next = temp;
+             cur = temp->next;
+         }
+         
+         // copy random pointer
+         cur = head;
+         while(cur != NULL)
+         {
+             RandomListNode* temp = cur->next;
+             if(cur->random != NULL)
+                 temp->random = cur->random->next;
+             cur = temp->next;
+         }
+         
+         //decouple two links
+         cur = head;
+         RandomListNode* dup = head == NULL? NULL:head->next;
+         while(cur != NULL)
+         {
+             RandomListNode* temp = cur->next;
+             cur->next = temp->next;
+             if(temp->next!=NULL)
+                 temp->next = temp->next->next;
+             cur = cur->next;
+         }
+ 
+         return dup;
+     }
 };
 
-RandomListNode* copyRandomList(RandomListNode* head) {
 
-	if (!head) { return NULL;}
-	map<RandomListNode*, RandomListNode*> mp;
-	mp.clear();
-	RandomListNode* res = new RandomListNode(0);
-	RandomListNode* p=head;
-	RandomListNode* q=res;
 
-	while(p) {
-		RandomListNode* tmp = new RandomListNode(p->label);
-		q->next = tmp;
-		mp[p] = tmp;
-		p = p->next;
-		q = q->next;
-	}
-	p = head;
-	q = res->next;
-	while(p){
-		if(p->random==NULL){
-			q->random = NULL;
-		} else {
-			q->random = mp[p->random];
-		}	
-		p = p->next;
-		q = q->next;
-	}
-	return res->next;
-}
+

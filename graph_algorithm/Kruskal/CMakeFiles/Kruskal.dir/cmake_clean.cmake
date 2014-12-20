7e7938c83bbd2e01509f7e0b97fd320e312464f3
dimitar9 <dimitar9909@gmail.com>
2014-12-20 00:47:16 -0500

    delete

---
+++ graph_algorithm/Kruskal/CMakeFiles/Kruskal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Kruskal.dir/Kruskal.cpp.o"
+  "Kruskal.pdb"
+  "Kruskal"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Kruskal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ graph_algorithm/Kruskal/CMakeFiles/Sample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Sample.dir/Sample.cpp.o"
+  "Sample.pdb"
+  "Sample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

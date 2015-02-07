9ff230e4beaf854f2c5adf40a14815176ba2b928
Dimitar9 <dimitar9909@gmail.com>
2015-02-07 16:03:46 -0500

    Create T147_insertionSortList.java

---
+++ leetcode_java/T147_insertionSortList.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+public ListNode insertionSortList(ListNode head) {
+    ListNode helper=new ListNode(0);
+    ListNode pre=helper;
+    ListNode current=head;
+    while(current!=null) {
+        pre=helper;
+        while(pre.next!=null&&pre.next.val<current.val) {
+            pre=pre.next;
+        }
+        ListNode next=current.next;
+        current.next=pre.next;
+        pre.next=current;
+        current=next;
+    }
+    return helper.next;
+}
+}

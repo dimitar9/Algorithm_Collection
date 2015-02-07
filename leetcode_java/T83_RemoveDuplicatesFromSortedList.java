0326b8b05ea700a130eb20757bc02b3d1f666c7b
Dimitar9 <dimitar9909@gmail.com>
2015-02-07 15:59:25 -0500

    Create T83_RemoveDuplicatesFromSortedList.java

---
+++ leetcode_java/T83_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+ 
+        ListNode prev = head;    
+        ListNode p = head.next;
+ 
+        while(p != null){
+            if(p.val == prev.val){
+                prev.next = p.next;
+                p = p.next;
+                //no change prev
+            }else{
+                prev = p;
+                p = p.next; 
+            }
+        }
+ 
+        return head;
+    }
+}

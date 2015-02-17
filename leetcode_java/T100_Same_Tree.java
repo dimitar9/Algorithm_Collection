18f50d5e403989da476a4a14b725030f024fb01a
Dimitar9 <dimitar9909@gmail.com>
2015-02-17 00:44:30 -0500

    Create T100_Same_Tree.java

---
+++ leetcode_java/T100_Same_Tree.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q == null) return true;
+        if(p!=null && q == null) return false;
+        if(p == null && q!= null) return false;
+        if ( (p.val == q.val) && (isSameTree(p.left,q.left)) &&(isSameTree(p.right,q.right)) )
+            return true;
+        else {
+            return false;
+        }
+        
+        
+    }
+}

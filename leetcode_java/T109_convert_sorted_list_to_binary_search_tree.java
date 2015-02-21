228d92335c6d76e5f7bb8163aa8e9d70b3c7336c
Dimitar9 <dimitar9909@gmail.com>
2015-02-21 14:04:39 -0500

    Update T109_convert_sorted_list_to_binary_search_tree.java

--- leetcode_java/T109_convert_sorted_list_to_binary_search_tree.java
+++ leetcode_java/T109_convert_sorted_list_to_binary_search_tree.java
@@ -1,36 +1,43 @@
-private ListNode node;
-
-public TreeNode sortedListToBST(ListNode head) {
-    if(head == null){
-        return null;
-    }
-
-    int size = 0;
-    ListNode runner = head;
-    node = head;
-
-    while(runner != null){
-        runner = runner.next;
-        size ++;
-    }
-
-    return inorderHelper(0, size - 1);
-}
-
-public TreeNode inorderHelper(int start, int end){
-    if(start > end){
-        return null;
-    }
-
-    int mid = start + (end - start) / 2;
-    TreeNode left = inorderHelper(start, mid - 1);
-
-    TreeNode treenode = new TreeNode(node.val);
-    treenode.left = left;
-    node = node.next;
-
-    TreeNode right = inorderHelper(mid + 1, end);
-    treenode.right = right;
-
-    return treenode;
+public class Solution {
+	static ListNode h;
+ 
+	public TreeNode sortedListToBST(ListNode head) {
+		if (head == null)
+			return null;
+ 
+		h = head;
+		int len = getLength(head);
+		return sortedListToBST(0, len - 1);
+	}
+ 
+	// get list length
+	public int getLength(ListNode head) {
+		int len = 0;
+		ListNode p = head;
+ 
+		while (p != null) {
+			len++;
+			p = p.next;
+		}
+		return len;
+	}
+ 
+	// build tree bottom-up
+	public TreeNode sortedListToBST(int start, int end) {
+		if (start > end)
+			return null;
+ 
+		// mid
+		int mid = (start + end) / 2;
+ 
+		TreeNode left = sortedListToBST(start, mid - 1);
+		TreeNode root = new TreeNode(h.val);
+		h = h.next;
+		TreeNode right = sortedListToBST(mid + 1, end);
+ 
+		root.left = left;
+		root.right = right;
+ 
+		return root;
+	}
 }

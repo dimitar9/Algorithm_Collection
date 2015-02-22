73247f6f502016fcdbbbd89d2b448efa22a619d4
Dimitar9 <dimitar9909@gmail.com>
2015-02-21 20:17:22 -0500

    Update and rename leetcode_java to leetcode_java/T112_Path_Sum.java

---
+++ leetcode_java/T112_Path_Sum.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        if(root.val==sum && root.left==null && root.right==null) return true;
+        else return (hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val));
+    }
+}

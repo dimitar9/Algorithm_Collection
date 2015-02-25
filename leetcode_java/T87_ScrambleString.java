48322fb260d8bf520fdcaa313e73ee99a4cabd60
Dimitar9 <dimitar9909@gmail.com>
2015-02-25 00:58:41 -0500

    Update T87_ScrambleString.java

--- leetcode_java/T87_ScrambleString.java
+++ leetcode_java/T87_ScrambleString.java
@@ -15,3 +15,26 @@ public class Solution {
         return false;
     }
 }
+
+//dp solution
+//A more concise C++ DP version. dp[i][j][l] means whether s2.substr(j,l) is a scrambled string of s1.substr(i,l) or not.
+
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int len=s1.size();
+        bool dp[100][100][100]={false};
+        for (int i=len-1;i>=0;i--)
+            for (int j=len-1;j>=0;j--) {
+                dp[i][j][1]=(s1[i]==s2[j]);
+                for (int l=2;i+l<=len && j+l<=len;l++) {
+                    for (int n=1;n<l;n++) {
+                        dp[i][j][l]|=dp[i][j][n]&&dp[i+n][j+n][l-n];
+                        dp[i][j][l]|=dp[i][j+l-n][n]&&dp[i+n][j][l-n];
+                    }
+                }
+            }
+        return dp[0][0][len];
+    }
+}; 
+

fcdce7a1fc1186b55db5f4d7fe299f2c08161f6f
Dimitar9 <dimitar9909@gmail.com>
2015-03-01 00:45:46 -0500

    Update T10_Regular_Expression_matching.java

--- leetcode_java/T10_Regular_Expression_matching.java
+++ leetcode_java/T10_Regular_Expression_matching.java
@@ -21,3 +21,43 @@ bool isMatch(const char *s, const char *p) {
 };
 
 */
+
+public class T10 {
+
+	static boolean   matchFirst(String s, String p){
+		System.out.println("in matchFirst: s is "+s+",p is: "+p+";");
+		System.out.println("s is empty "+ s.isEmpty());
+		System.out.println("p is empty "+ p.isEmpty());
+        if(s.isEmpty() ^ p.isEmpty()) return false;
+        if(s.isEmpty() && p.isEmpty()) return true;
+        return ( s.charAt(0)==p.charAt(0) || p.charAt(0)=='.' );
+    }
+    public static boolean isMatch(String s, String p) {
+    	if (p.isEmpty() )return (s.isEmpty());
+       
+
+        
+        if(p.length()==1 ||  p.charAt(1)!='*') {
+            if(!matchFirst(s,p)) return false;
+            return isMatch(s.substring(1), p.substring(1));
+        } else {
+        	
+            if (isMatch(s, p.substring(2))) return true;
+            while(true){
+                if(isMatch(s, p.substring(2))) return true;
+            	if(!matchFirst(s, p)) break;
+                s=s.substring(1);
+            }
+        }
+        return false;
+    }
+
+    
+    
+    public static void main(String[] args){
+    	String s = "aaa";
+    	String p = "ab*a";
+    	boolean ret = isMatch(s, p);
+    	System.out.println(ret);
+    }
+}

ff871052d81af764af2278d0c3038f8837806991
dimitar9 <dimitar9909@gmail.com>
2015-03-05 21:31:10 -0500

    s

--- topcoder/StrangeCountry.cpp
+++ topcoder/StrangeCountry.cpp
@@ -55,6 +55,7 @@ public:
 	
 	int transform(vector <string> g) {
 		this->g = g;
+
 		memset(usedV, false, sizeof(usedV));
 		memset(usedE, false, sizeof(usedE));
 		
@@ -81,10 +82,40 @@ public:
 	
 	// BEGIN CUT HERE
 public:
-	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	void run_test(int Case) 
+	{ 
+		if ((Case == -1) || (Case == 0)) 
+			test_case_0();
+		if ((Case == -1) || (Case == 1)) 
+			test_case_1(); 
+		if ((Case == -1) || (Case == 2)) 
+			test_case_2(); 
+		if ((Case == -1) || (Case == 3)) 
+			test_case_3(); 
+		if ((Case == -1) || (Case == 4)) 
+			test_case_4(); 
+		if ((Case == -1) || (Case == 5)) 
+			test_case_5(); 
+	}
 private:
-	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
-	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	template <typename T> string print_array(const vector<T> &V) 
+		{ ostringstream os; 
+			os << "{ "; 
+			for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) 
+				os << '\"' << *iter << "\","; os << " }"; 
+			return os.str(); 
+		}
+	
+	void verify_case(int Case, const int &Expected, const int &Received) 
+		{ cerr << "Test Case #" << Case << "..."; 
+		  if (Expected == Received) 
+		  	cerr << "PASSED" << endl;
+		  else { 
+		  	cerr << "FAILED" << endl; 
+		  	cerr << "\tExpected: \"" << Expected << '\"' << endl; 
+		  	cerr << "\tReceived: \"" << Received << '\"' << endl; 
+		  } 
+		}
 	void test_case_0() { string Arr0[] = {"NY",
 		"YN"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(0, Arg1, transform(Arg0)); }
 	void test_case_1() { string Arr0[] = {"NYYNN",

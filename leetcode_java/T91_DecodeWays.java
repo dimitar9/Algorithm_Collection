fbf0b4b02a88e8e74c13c0df8e0ccb39ae80ff07
Dimitar9 <dimitar9909@gmail.com>
2015-03-08 19:22:00 -0400

    Update T91_DecodeWays.java

--- leetcode_java/T91_DecodeWays.java
+++ leetcode_java/T91_DecodeWays.java
@@ -2,16 +2,25 @@ public class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         if (n == 0) return 0;
-
-        int[] memo = new int[n+1];
-        memo[n]  = 1;
-        memo[n-1] = s.charAt(n-1) != '0' ? 1 : 0;
-
-        for (int i = n - 2; i >= 0; i--)
-            if (s.charAt(i) == '0') continue;
-            else memo[i] = (Integer.parseInt(s.substring(i,i+2))<=26) ? memo[i+1]+memo[i+2] : memo[i+1];
-
-        return memo[0];
+    
+        int[] dp = new int[n+1];
+        dp[n] = 1;
+        if(s.charAt(n-1) != '0')    
+            dp[n-1] = 1;
+        else 
+            dp[n-1] = 0;
+        for(int i = n-2; i >=0; i--){
+            if (s.charAt(i) =='0') continue;
+            if(Integer.parseInt(s.substring(i,i+2)) <= 26 )
+            {
+                dp[i] = dp[i+1] + dp[i+2];
+            } else {
+                dp[i] = dp[i+1];
+            }
+            
+        }
+        return dp[0];
+        
     }
 }
 

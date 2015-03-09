415efab5be5724535d7299e42a39f2a9baada0a3
Dimitar9 <dimitar9909@gmail.com>
2015-03-09 00:50:08 -0400

    Create T152_maximum_product_subarray.java

---
+++ leetcode_java/T152_maximum_product_subarray.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxProduct(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int max = A[0], min = A[0], result = A[0];
+        for (int i = 1; i < A.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+    }
+}

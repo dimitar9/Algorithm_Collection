0b6078be33319bfa8acf0d4643df5c4476c0359d
Dimitar9 <dimitar9909@gmail.com>
2015-03-09 11:15:44 -0400

    Update T89_Gray_code.java

--- leetcode_java/T89_Gray_code.java
+++ leetcode_java/T89_Gray_code.java
@@ -1,11 +1,11 @@
 public class Solution {
 	long gennext(int bi){
-		return (bi >>> 1) ^ bi;
+		return (bi >>> 1) ^ bi; // This is the key
 	}
     public List<Integer> grayCode(int n) {
     	
         List<Integer> ret = new ArrayList<Integer>();
-        for(int i = 0; i < Math.pow(2, n); i++){
+        for(int i = 0; i < Math.pow(2, n); i++){  // careful about end point. it is pow(2,n) not n
         	ret.add((int) gennext(i));
         }
         

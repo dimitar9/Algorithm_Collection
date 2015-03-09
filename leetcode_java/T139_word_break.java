61648040f2954763e7fccc29ec9f1e3d4e546beb
Dimitar9 <dimitar9909@gmail.com>
2015-03-08 23:50:34 -0400

    Create T139_word_break.java

---
+++ leetcode_java/T139_word_break.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public boolean wordBreak(String s, Set<String> dict) {
+
+        boolean[] f = new boolean[s.length() + 1];
+        Arrays.fill(f, false);
+
+        f[0] = true;
+
+
+        /* First DP
+        for(int i = 1; i <= s.length(); i++){
+            for(String str: dict){
+                if(str.length() <= i){
+                    if(f[i - str.length()]){
+                        if(s.substring(i-str.length(), i).equals(str)){
+                            f[i] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }*/
+
+        //Second DP
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && dict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[s.length()];
+    }
+}

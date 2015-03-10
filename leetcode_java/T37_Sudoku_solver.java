64ef64ba0936d99050688db157a978fa602885cb
Dimitar9 <dimitar9909@gmail.com>
2015-03-10 01:44:53 -0400

    Update T37_Sudoku_solver.java

--- leetcode_java/T37_Sudoku_solver.java
+++ leetcode_java/T37_Sudoku_solver.java
@@ -1,57 +1,84 @@
-class Solution {
-public:
-    void solveSudoku(vector<vector<char> > &board) {
-        if(board.size()<9 || board[0].size()<9) return;
-        bool findSol = solSudoku(board, 0, 0);
+/*
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+Empty cells are indicated by the character '.'.
+
+You may assume that there will be only one unique solution.
+
+
+*/
+
+public class SudokuSolver {
+    public void solveSudoku(char[][] board) {
+        if (board==null||board.length==0){
+            return;
+        }
+        
+        solved(board);
+        
+        
     }
-    
-    bool solSudoku(vector<vector<char>> &board, int irow, int icol) {
-        if(irow==9) return true;
-        
-        int irow2, icol2;
-        if(icol==8) {
-            irow2 = irow+1;
-            icol2 = 0;
-        }
-        else {
-            irow2 = irow;
-            icol2 = icol+1;
-        }
-        
-        if(board[irow][icol]!='.') {
-            if(!isValid(board, irow, icol)) return false;
-            return solSudoku(board, irow2, icol2);
-        }
-        
-        for(int i=1; i<=9; i++) {
-            board[irow][icol] = '0'+i;
-            if(isValid(board, irow, icol) && solSudoku(board, irow2, icol2)) return true;
-        }
-        
-        // reset grid 
-        board[irow][icol] = '.';
-        return false;
-    }
-    
-    bool isValid(vector<vector<char>> &board, int irow, int icol) {
-        char val = board[irow][icol];
-        if(val-'0'<1 || val-'0'>9) return false;
-        
-        // check row & col
-        for(int i=0; i<9; i++) {
-            if(board[irow][i]==val && i!=icol) return false;
-            if(board[i][icol]==val && i!=irow) return false;
-        }
-        
-        //check 3x3 box
-        int irow0 = irow/3*3;
-        int icol0 = icol/3*3;
-        for(int i=irow0; i<irow0+3; i++) {
-            for(int j=icol0; j<icol0+3; j++) {
-                if(board[i][j]==val && (i!=irow || j!=icol)) return false;
+    private boolean solved(char[][] board){
+      
+        for(int i=0; i<board.length; i++){
+            for (int j=0; j<board[0].length; j++){
+                if (board[i][j]=='.'){
+                    for (char num='1'; num<='9'; num++){
+                        
+                        if(isValid(board, i, j, num)){
+                            // no conflict
+                            board[i][j]=num;
+                            
+                            if (solved(board)){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                            
+                        }
+                      
+                    }
+                    // if no proper number found, return false
+                    return false;
+                }
             }
         }
         
         return true;
     }
-};
+    
+    private boolean isValid(char[][] board, int i, int j, char c){
+     
+        // check column
+        for (int row=0; row<9; row++){
+            if (board[row][j]==c){
+                return false;
+            }
+           
+             
+        }
+        
+       // check row
+        for (int col=0; col<9; col++){
+            if (board[i][col]==c){
+                return false;
+            }
+            
+        }
+      
+        // check block
+        for(int row=i/3*3; row<i/3*3+3; row++){
+            for (int col=j/3*3; col<j/3*3+3; col++){
+                if (board[row][col]==c){
+                    return false;
+                }
+                
+            }
+        }
+       
+        return true;
+        
+    }
+}
+

860692107c4679fd5f975b4c8560a9a9926231a8
Dimitar9 <dimitar9909@gmail.com>
2015-03-10 00:23:34 -0400

    Create T17_letter_combination_of_a_phone_number.java

---
+++ leetcode_java/T17_letter_combination_of_a_phone_number.java
@@ -0,0 +1,26 @@
+public class Solution {
+	    public List<String> letterCombinations(String digits) {
+	        
+	   	 	List<String> ret = new ArrayList<>();
+	   	 	List<String> pad = Arrays.asList("","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" );
+	        String s = "";
+	        for(int i = 0 ;i < digits.length(); i++){
+	        	gen(s, i, digits.length(), pad, ret, digits);
+	        }
+	        return ret;
+	    }
+	    public  void gen(String s, int k, int len, List<String> pad, List<String> ret,String digits){
+	    	if(s.length() ==len ){
+	    		ret.add(s);
+	    		return;
+	    	}
+	    	if(k>=digits.length()) return;
+	    	for(int i = 0; i < pad.get(digits.charAt(k)-'0').length(); i++){
+	    		 s += pad.get(digits.charAt(k)-'0').charAt(i);
+	    		
+	    		 gen(s, k+1, len, pad, ret,digits);
+	    		 s = s.substring(0,s.length()-1);
+	        
+	    	}
+	    }
+}

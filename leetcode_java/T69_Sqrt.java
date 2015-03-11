88e5d40d45eae50b6a7b47a097abdb93c71ab5c6
Dimitar9 <dimitar9909@gmail.com>
2015-03-10 22:49:59 -0400

    Create T69_Sqrt.java

---
+++ leetcode_java/T69_Sqrt.java
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sqrt(int x) {
+        if (0 == x) return 0;
+        int left = 1, right = x, ans;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//binary search

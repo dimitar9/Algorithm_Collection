92ef42a995cd2a68141b81eb3f848d2d7a6cdced
Dimitar9 <dimitar9909@gmail.com>
2015-03-13 00:03:44 -0400

    Create T60_permutation_sequence.cpp

---
+++ leetcode_java/T60_permutation_sequence.cpp
@@ -0,0 +1,20 @@
+string getPermutation(int n, int k) {
+    int i,j,f=1;
+    // left part of s is partially formed permutation, right part is the leftover chars.
+    string s(n,'0');
+    for(i=1;i<=n;i++){
+        f*=i;
+        s[i-1]+=i; // make s become 1234...n
+    }
+    for(i=0,k--;i<n;i++){
+        f/=n-i;
+        j=i+k/f; // calculate index of char to put at s[i]
+        char c=s[j];
+        // remove c by shifting to cover up (adjust the right part).
+        for(;j>i;j--)
+            s[j]=s[j-1];
+        k%=f;
+        s[i]=c;
+    }
+    return s;
+}

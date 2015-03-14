241221e2a01d499403bf75f481de142d9139c789
Dimitar9 <dimitar9909@gmail.com>
2015-03-14 15:01:03 -0400

    Update T78_Subsets.java

--- leetcode_java/T78_Subsets.java
+++ leetcode_java/T78_Subsets.java
@@ -15,3 +15,33 @@ public class Solution {
         return solutions;
     }
 }
+
+
+//my solution
+public class Solution {
+    public List<List<Integer>> subsets(int[] S) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        int retn = (int) Math.pow(2, S.length);
+        Arrays.sort(S);
+        List<Integer> empty = new ArrayList<Integer>();
+        ret.add(empty);
+        for(int i = 1; i< retn; i++){
+            ret.add(getList(i, S));
+        }
+        return ret;
+    }
+    List<Integer> getList(int k , int[] S){
+        List<Integer> l = new ArrayList<Integer>();
+        int i = 0;
+        int len = S.length;
+        while((k  > 0) &&(i < len)){
+            if((k & 1) == 1) {
+                l.add(S[i]);
+               
+            }
+            i++;
+            k = k >>> 1;
+        } 
+        return l;
+    }
+}

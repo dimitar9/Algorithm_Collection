2e916ec8226c97fa1e70551a481f3077e85a8fc2
Dimitar9 <dimitar9909@gmail.com>
2015-03-30 01:41:24 -0400

    Create T54_spiral_matrix.java

---
+++ leetcode_java/T54_spiral_matrix.java
@@ -0,0 +1,60 @@
+
+/*This is a very simple and easy to understand solution. 
+I traverse right and increment rowBegin, then traverse down 
+and decrement colEnd, then I traverse left and decrement 
+rowEnd, and finally I traverse up and increment colBegin.
+
+The only tricky part is that when I traverse left or up 
+I have to check whether the row or col still exists to
+prevent duplicates. If anyone can do the same thing without
+that check, please let me know!
+
+Any comments greatly appreciated.
+*/
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> res = new ArrayList<Integer>();
+
+        if (matrix.length == 0) {
+            return res;
+        }
+
+        int rowBegin = 0;
+        int rowEnd = matrix.length-1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // Traverse Right
+            for (int j = colBegin; j <= colEnd; j ++) {
+                res.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+
+            // Traverse Down
+            for (int j = rowBegin; j <= rowEnd; j ++) {
+                res.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin <= rowEnd) {
+                // Traverse Left
+                for (int j = colEnd; j >= colBegin; j --) {
+                    res.add(matrix[rowEnd][j]);
+                }
+            }
+            rowEnd--;
+
+            if (colBegin <= colEnd) {
+                // Traver Up
+                for (int j = rowEnd; j >= rowBegin; j --) {
+                    res.add(matrix[j][colBegin]);
+                }
+            }
+            colBegin ++;
+        }
+
+        return res;
+    }
+}

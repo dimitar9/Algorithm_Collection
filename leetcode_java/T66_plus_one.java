4f73e11da7fdd2e25fc31ce729e3097a8e7bdd05
Dimitar9 <dimitar9909@gmail.com>
2015-03-31 23:16:15 -0400

    Update and rename t66_plus_one.java to T66_plus_one.java

--- leetcode_java/T66_plus_one.java
+++ leetcode_java/T66_plus_one.java
@@ -33,3 +33,28 @@ public class Solution {
         
     }
 }
+////////////////
+////////////////
+//better one
+
+
+
+void plusone(vector<int> &digits)
+{
+    int n = digits.size();
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (digits[i] == 9)
+        {
+            digits[i] = 0;
+        }
+        else
+        {
+            digits[i]++;
+            return;
+        }
+    }
+        digits[0] =1;
+        digits.push_back(0);
+
+}

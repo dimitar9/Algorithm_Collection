f8195dc903a7503e57f3ac73db1ccff81e0aa6e2
Dimitar9 <dimitar9909@gmail.com>
2015-03-31 22:30:55 -0400

    Update T64_minimum_path_sum.java

--- leetcode_java/T64_minimum_path_sum.java
+++ leetcode_java/T64_minimum_path_sum.java
@@ -9,7 +9,7 @@ public class Solution {
         dp[0][0] = grid[0][0];
         for(int i = 1; i < m; i++)
         {
-            dp[i][0] = dp[i-1][0]+grid[i][0];
+            dp[i][0] = dp[i-1][0]+grid[i][0];//important. Note: don;t use grid do +=.
         }
         for(int j = 1; j < n; j++)
         {
@@ -17,7 +17,7 @@ public class Solution {
         }
         for(int i = 1; i < m; i++){
             for(int j =1; j < n; j++){
-                dp[i][j] = Math.min(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j]);
+                dp[i][j] = Math.min(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j]); //important
             }
         }
         return dp[m-1][n-1];

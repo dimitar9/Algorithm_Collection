bf94044134f8545fd26b5d2d4130a012cfcdbf19
Dimitar9 <dimitar9909@gmail.com>
2015-04-02 23:00:18 -0400

    Create HappyNumber.java

---
+++ LeetcodeNewPrivate/HappyNumber.java
@@ -0,0 +1,20 @@
+/*Happy Number
+Postby 1337c0d3r » Mon Mar 02, 2015 2:55 pm
+
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: 
+Starting with any positive integer, replace the number by the sum of 
+the squares of its digits, and repeat the process until the number equals 1 
+(where it will stay), or it loops endlessly in a cycle which does not include 1. 
+Those numbers for which this process ends in 1 are happy numbers, while those that 
+do not end in 1 are unhappy numbers (or sad numbers)
+
+
+An example of a happy number is 19: 
+
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+*/

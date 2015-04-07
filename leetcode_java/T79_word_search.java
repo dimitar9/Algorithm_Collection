619c1b89accc467e09001708453bfb24087a861d
Dimitar9 <dimitar9909@gmail.com>
2015-04-07 01:33:22 -0400

    Update T79_word_search.java

--- leetcode_java/T79_word_search.java
+++ leetcode_java/T79_word_search.java
@@ -13,7 +13,7 @@ private boolean exist(char[][] board, int y, int x, char[] word, int i) {
     if (i == word.length) return true;
     if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
     if (board[y][x] != word[i]) return false;
-    board[y][x] ^= 256;
+    board[y][x] ^= 256; //because ASCII has 256. so the values are all less than 256.
     boolean exist = exist(board, y, x+1, word, i+1)
         || exist(board, y, x-1, word, i+1)
         || exist(board, y+1, x, word, i+1)
@@ -21,8 +21,4 @@ private boolean exist(char[][] board, int y, int x, char[] word, int i) {
     board[y][x] ^= 256;
     return exist;
 }
-    
-    
-    
-    
 }

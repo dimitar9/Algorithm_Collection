677f8b1826fe3d302d0a96a62ec7171a149b536e
Dimitar9 <dimitar9909@gmail.com>
2015-04-11 23:44:35 -0400

    Update T90_subsets_2.java

--- leetcode_java/T90_subsets_2.java
+++ leetcode_java/T90_subsets_2.java
@@ -1,13 +1,5 @@
-public class Subsets2 {
-    
-    public static void main(String[] args) {
-        int[] S = {1, 2, 2, 2};
-        System.out.println(subsetsWithDup(S));
-    }
-    
-    
-    public static ArrayList<ArrayList<Integer>> subsetsWithDup (int[] num) {
-        
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] num) {
         Arrays.sort(num);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         List<Integer> list = new ArrayList<Integer>();
@@ -16,20 +8,19 @@ public class Subsets2 {
  
         return result;
     }
- 
- 
-    private static void subsetsWithDupHelper(ArrayList<ArrayList<Integer>> result,
-            ArrayList<Integer> list, int[] num, int position) {
-        result.add(new ArrayList<Integer>(list));
- 
-        for (int i = position; i < num.length; i++) {
-            if (i != position && num[i] == num[i-1]){
-                continue;
-            }
+    
+        private static void subsetsWithDupHelper(List<List<Integer>> result, List<Integer> list, int[] num, int position) {
             
-            list.add(num[i]);
-            subsetsWithDupHelper(result, list, num, i+1);
-            list.remove(list.size() - 1);
-        }
+            result.add(new ArrayList<Integer>(list));
+     
+            for (int i = position; i < num.length; i++) {
+                if (i != position && num[i] == num[i-1]){
+                    continue;
+                }
+                
+                list.add(num[i]);
+                subsetsWithDupHelper(result, list, num, i+1);
+                list.remove(list.size() - 1);
+            }
     }
 }

2ed96e66d2391012a4d11bdd61dc8a3d11f76a28
Dimitar9 <dimitar9909@gmail.com>
2015-04-20 18:09:15 -0400

    Update FlattenIterator.java

--- MianJing/FlattenIterator.java
+++ MianJing/FlattenIterator.java
@@ -2,3 +2,6 @@
 //Given a list which can can contain elements as well as lists,
 //write an iterator to flatten a nested list. Please make this code better and suggest any improvements.
 //http://codereview.stackexchange.com/questions/32827/flatten-iterator-for-nested-list
+
+//Google iterator: https://code.google.com/p/google-collections/source/browse/trunk/src/com/google/common/collect/Iterators.java?r=4
+//composite pattern: https://en.wikipedia.org/wiki/Composite_pattern

8647a1360b1f403de3a4b3786612de682d325057
Dimitar9 <dimitar9909@gmail.com>
2015-05-19 14:43:10 -0400

    Create T92_reverse_linked_list_2.java

---
+++ leetcode_java/T92_reverse_linked_list_2.java
@@ -0,0 +1,29 @@
+//Simply just reverse the list along the way using 4 pointers: dummy, pre, start, then
+
+public ListNode reverseBetween(ListNode head, int m, int n) {
+    if(head == null) return null;
+    ListNode dummy = new ListNode(0); // create a dummy node to mark the head of this list
+    dummy.next = head;
+    ListNode pre = dummy; // make a pointer pre as a marker for the node before reversing
+    for(int i = 0; i<m-1; i++) pre = pre.next;
+
+    ListNode start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
+    ListNode then = start.next; // a pointer to a node that will be reversed
+
+    // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+    // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+
+    for(int i=0; i<n-m; i++)
+    {
+        start.next = then.next;
+        then.next = pre.next;
+        pre.next = then;
+        then = start.next;
+    }
+
+    // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+    // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+
+    return dummy.next;
+
+}

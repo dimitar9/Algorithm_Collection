319acd5e307206134603466d44eeb37e2ecb524a
Dimitar9 <dimitar9909@gmail.com>
2015-05-26 21:50:23 -0400

    Update T71_simplify_path.java

--- leetcode_java/T71_simplify_path.java
+++ leetcode_java/T71_simplify_path.java
@@ -1,13 +1,13 @@
 public class Solution {
 public String simplifyPath(String path) {
-    Deque<String> stack = new LinkedList<>();
+    Stack<String> stack = new Stack<>();
     Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
     for (String dir : path.split("/")) {
         if (dir.equals("..") && !stack.isEmpty()) stack.pop();
         else if (!skip.contains(dir)) stack.push(dir);
     }
     String res = "";
-    for (String dir : stack) res = "/" + dir + res;
+    for (String dir : stack) res = res + "/" + dir; 
     return res.isEmpty() ? "/" : res;
 }
 

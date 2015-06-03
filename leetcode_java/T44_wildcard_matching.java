0076b5113eb0f82fa75329cc48f31fcb9c01bd84
Dimitar9 <dimitar9909@gmail.com>
2015-06-02 23:14:02 -0400

    Create T44_wildcard_matching.java

---
+++ leetcode_java/T44_wildcard_matching.java
@@ -0,0 +1,33 @@
+public class Solution {
+boolean comparison(String str, String pattern) {
+        int s = 0, p = 0, match = 0, starIdx = -1;            
+        while (s < str.length()){
+            // advancing both pointers
+            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
+                s++;
+                p++;
+            }
+            // * found, only advancing pattern pointer
+            else if (p < pattern.length() && pattern.charAt(p) == '*'){
+                starIdx = p;
+                match = s;
+                p++;
+            }
+           // last pattern pointer was *, advancing string pointer
+            else if (starIdx != -1){
+                p = starIdx + 1;
+                match++;
+                s = match;
+            }
+           //current pattern pointer is not star, last patter pointer was not *
+          //characters do not match
+            else return false;
+        }
+
+        //check for remaining characters in pattern
+        while (p < pattern.length() && pattern.charAt(p) == '*')
+            p++;
+
+        return p == pattern.length();
+}
+}

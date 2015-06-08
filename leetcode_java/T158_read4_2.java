4a33684f11b2a8731056a1f41f99cd6dbf88926e
Dimitar9 <dimitar9909@gmail.com>
2015-06-08 00:11:25 -0400

    Create T158_read4_2.java

---
+++ leetcode_java/T158_read4_2.java
@@ -0,0 +1,22 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    private int buffPtr = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            if (buffPtr == 0) {
+                buffCnt = read4(buff);
+            }
+            if (buffCnt == 0) break;
+            while (ptr < n && buffPtr < buffCnt) {
+                buf[ptr++] = buff[buffPtr++];
+            }
+            if (buffPtr >= buffCnt) buffPtr = 0;
+        }
+        return ptr;
+    }
+}

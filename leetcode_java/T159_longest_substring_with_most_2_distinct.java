06ebad429d1c604096a93d938d425e7412e02ca4
Dimitar9 <dimitar9909@gmail.com>
2015-06-08 00:54:37 -0400

    Create T159_longest_substring_with_most_2_distinct.java

---
+++ leetcode_java/T159_longest_substring_with_most_2_distinct.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public static int lengthOfLongestSubstringTwoDistinct(String s) {
+    	HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
+        int len = s.length();
+        int j = 0;
+        int maxlen = 0;
+        int curDistinctCount=0;
+        for(int i =0; i < len; i++){
+            if(hm.get(s.charAt(i))==null || hm.get(s.charAt(i))==0){
+                hm.put(s.charAt(i), 1);
+                curDistinctCount++;
+            } else {
+                hm.put(s.charAt(i), hm.get(s.charAt(i))+1);
+            }
+            if(curDistinctCount<=2){
+                maxlen = Math.max(maxlen, i-j+1);
+            } else {
+                while(j<s.length() && hm.get(s.charAt(j))!=null && hm.get(s.charAt(j))!=1){
+                    hm.put(s.charAt(j),hm.get(s.charAt(j))-1);
+                    j++;
+                } 
+                if(j>=len) break;
+                hm.put(s.charAt(j),0);
+                curDistinctCount--;
+                j++;
+            }
+        }
+        return maxlen;
+    }
+}

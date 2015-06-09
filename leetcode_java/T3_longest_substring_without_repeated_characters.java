5dfa78e73b70bced96f1add0f35afeb391b5f2a5
Dimitar9 <dimitar9909@gmail.com>
2015-06-08 21:38:45 -0400

    Update T3_longest_substring_without_repeated_characters.java

--- leetcode_java/T3_longest_substring_without_repeated_characters.java
+++ leetcode_java/T3_longest_substring_without_repeated_characters.java
@@ -1,16 +1,16 @@
 public class Solution {
-   public int lengthOfLongestSubstring(String s) {
-        if (s.length()==0) return 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max=0;
-        for (int i=0, j=0; i<s.length(); ++i){
-            if (map.containsKey(s.charAt(i))){
-                j = Math.max(j,map.get(s.charAt(i))+1);
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0) return 0;
+        Map<Character,Integer> map = new HashMap<>();
+        int max = 0;
+        for(int i = 0, j = 0; j < s.length(); j++){
+            if(map.containsKey(s.charAt(j))){
+                i = Math.max(i, map.get(s.charAt(j))+1);
             }
-            map.put(s.charAt(i),i);
-            max = Math.max(max,i-j+1);
+            map.put(s.charAt(j), j);
+            max = Math.max(j-i+1, max);
         }
         return max;
+        
     }
-
 }

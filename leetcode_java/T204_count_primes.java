85b3c5439230d4434187070c1238c5d0af346312
Dimitar9 <dimitar9909@gmail.com>
2015-06-14 14:22:19 -0400

    Update T204_count_primes.java

--- leetcode_java/T204_count_primes.java
+++ leetcode_java/T204_count_primes.java
@@ -3,16 +3,16 @@ public class Solution {
         int res = 0;
         boolean[] used = new boolean[n];
         for (int i = 2; i <= Math.sqrt(n); i++) {
-         if (!used[i - 1]) {
+         if (!used[i]) {
             int temp = i * i;
             while (temp < n) {
-                used[temp - 1] = true;
+                used[temp] = true;
                 temp += i;
             }
         }
         }
         for (int i = 2; i < n; i++) {
-        if (!used[i - 1]) {
+        if (!used[i]) {
             res++;
         }
         }

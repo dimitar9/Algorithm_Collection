e2f33f64efc2faf4ffae280e6e5c681ba0df3444
Dimitar9 <dimitar9909@gmail.com>
2015-06-14 15:01:00 -0400

    Update T205_isomophic_string.java

--- leetcode_java/T205_isomophic_string.java
+++ leetcode_java/T205_isomophic_string.java
@@ -1,12 +1,15 @@
-class Solution {
-public:
-    bool isIsomorphic(string s, string t) {
-        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] m1 = new int[256]; 
+        int[] m2 = new int[256];  
+        int n = s.length();
         for (int i = 0; i < n; ++i) {
-            if (m1[s[i]] != m2[t[i]]) return false;
-            m1[s[i]] = i + 1;
-            m2[t[i]] = i + 1;
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+            m1[s.charAt(i)] = i+1 ;
+            m2[t.charAt(i)] = i+1 ;
         }
         return true;
     }
-};
+    
+      
+}

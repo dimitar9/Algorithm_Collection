d5b692232c8256f3950a87bf7b11f4241c501fe4
Dimitar9 <dimitar9909@gmail.com>
2015-06-15 17:30:58 -0400

    Update T43_multiply_strings.java

--- leetcode_java/T43_multiply_strings.java
+++ leetcode_java/T43_multiply_strings.java
@@ -3,6 +3,13 @@ public String multiply(String num1, String num2) {
     int len1 = num1.length();
     int len2 = num2.length();
     int[] product = new int[len1 + len2];
+    
+    /// Insert in reversed order.
+    // for example  24 * 12
+    // +--+---+----+---+
+    // |8 | 8 | 2  |   |
+    // +--+---+----+---+
+
     for (int i = len1 - 1; i >= 0; i--) {
         for (int j = len2 - 1; j >= 0; j--) {
             int index = len1 + len2 - i - j - 2;

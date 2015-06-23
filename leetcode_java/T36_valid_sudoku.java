ef37706a7979e913bbb0423fdf393f5e84c1d0de
Dimitar9 <dimitar9909@gmail.com>
2015-06-23 00:57:00 -0400

    Update T36_valid_sudoku.java

--- leetcode_java/T36_valid_sudoku.java
+++ leetcode_java/T36_valid_sudoku.java
@@ -1,24 +1,20 @@
 public class Solution {
 public boolean isValidSudoku(char[][] board) {
-    for (int i=0; i<9; i++) {
-        if (!isParticallyValid(board,i,0,i,8)) return false;
-        if (!isParticallyValid(board,0,i,8,i)) return false;
-    }
-    for (int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if (!isParticallyValid(board,i*3,j*3,i*3+2,j*3+2)) return false;
+    for(int i = 0; i<9; i++){
+        HashSet<Character> rows = new HashSet<Character>();
+        HashSet<Character> columns = new HashSet<Character>();
+        HashSet<Character> cube = new HashSet<Character>();
+        for (int j = 0; j < 9;j++){
+            if(board[i][j]!='.' && !rows.add(board[i][j]))
+                return false;
+            if(board[j][i]!='.' && !columns.add(board[j][i]))
+                return false;
+            int RowIndex = 3*(i/3);
+            int ColIndex = 3*(i%3);
+            if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
+                return false;
         }
     }
     return true;
 }
-private boolean isParticallyValid(char[][] board, int x1, int y1,int x2,int y2){
-    Set singleSet = new HashSet();
-    for (int i= x1; i<=x2; i++){
-        for (int j=y1;j<=y2; j++){
-            if (board[i][j]!='.') if(!singleSet.add(board[i][j])) return false;
-        }
-    }
-    return true;
-}
-
 }

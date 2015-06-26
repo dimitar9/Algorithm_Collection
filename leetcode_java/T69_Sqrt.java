13c8471c421e499f95115d8b9800c85a9231998c
Dimitar9 <dimitar9909@gmail.com>
2015-06-25 22:09:23 -0400

    Update T69_Sqrt.java

--- leetcode_java/T69_Sqrt.java
+++ leetcode_java/T69_Sqrt.java
@@ -17,3 +17,21 @@ public:
 };
 
 //binary search
+public class Solution {
+public int sqrt(int x) {
+    if (x == 0)
+        return 0;
+    int left = 1, right = Integer.MAX_VALUE;
+    while (true) {
+        int mid = left + (right - left)/2;
+        if (mid > x/mid) {
+            right = mid - 1;
+        } else {
+            if (mid + 1 > x/(mid + 1))
+                return mid;
+            left = mid + 1;
+        }
+    }
+}
+
+}

5963ca04c2e29b4d1ca41b713e93dc36375b791d
Dimitar9 <dimitar9909@gmail.com>
2015-07-01 01:53:20 -0400

    Update T117_populating_next_right_pointer_for_each_node.java

--- leetcode_java/T117_populating_next_right_pointer_for_each_node.java
+++ leetcode_java/T117_populating_next_right_pointer_for_each_node.java
@@ -1,42 +1,15 @@
 public class Solution {
-//o1 space, o(n) time.
-    //based on level order traversal
     public void connect(TreeLinkNode root) {
 
-        TreeLinkNode head = null; //head of the next level
-        TreeLinkNode prev = null; //the leading node on the next level
-        TreeLinkNode cur = root;  //current node of current level
-
-        while (cur != null) {
-
-            while (cur != null) { //iterate on the current level
-                //left child
-                if (cur.left != null) {
-                    if (prev != null) {
-                        prev.next = cur.left;
-                    } else {
-                        head = cur.left;
-                    }
-                    prev = cur.left;
-                }
-                //right child
-                if (cur.right != null) {
-                    if (prev != null) {
-                        prev.next = cur.right;
-                    } else {
-                        head = cur.right;
-                    }
-                    prev = cur.right;
-                }
-                //move to next node
-                cur = cur.next;
+        while(root != null){
+            TreeLinkNode tempChild = new TreeLinkNode(0);
+            TreeLinkNode currentChild = tempChild;
+            while(root!=null){ //one level
+                if(root.left != null) { currentChild.next = root.left; currentChild = currentChild.next;}
+                if(root.right != null) { currentChild.next = root.right; currentChild = currentChild.next;}
+                root = root.next;
             }
-
-            //move to next level
-            cur = head;
-            head = null;
-            prev = null;
+            root = tempChild.next;
         }
-
     }
 }

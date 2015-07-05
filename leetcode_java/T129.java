d7418fa7f1cf9d59acaee6cc79563ab6c90b1169
Dimitar9 <dimitar9909@gmail.com>
2015-07-04 22:53:10 -0400

    Create T129.java

---
+++ leetcode_java/T129.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+public int sumNumbers(TreeNode root) {
+    return sum(root, 0);
+}
+
+public int sum(TreeNode n, int s){
+    if (n == null) return 0;
+    if (n.right == null && n.left == null) return s*10 + n.val;
+    return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+}
+
+}

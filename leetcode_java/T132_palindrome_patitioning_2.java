6273feaaea0ce0d5d6fc54d1ef139c9bcc08bdad
Dimitar9 <dimitar9909@gmail.com>
2015-07-05 11:47:28 -0400

    Update T132_palindrome_patitioning_2.java

--- leetcode_java/T132_palindrome_patitioning_2.java
+++ leetcode_java/T132_palindrome_patitioning_2.java
@@ -1,17 +1,13 @@
-class Solution {
-public:
-    int minCut(string s) {
-        int n = s.size();
-        vector<int> cut(n+1, 0);  // number of cuts for the first k characters
-        for (int i = 0; i <= n; i++) cut[i] = i-1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; i-j >= 0 && i+j < n && s[i-j]==s[i+j] ; j++) // odd length palindrome
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j]);
-
-            for (int j = 1; i-j+1 >= 0 && i+j < n && s[i-j+1] == s[i+j]; j++) // even length palindrome
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j+1]);
+public class Solution {
+ public int minCut(String s) {
+        if(s.length()==0)return 0;
+        int[]c=new int[s.length()+1];
+        for(int i=0;i<s.length();i++)c[i]=Integer.MAX_VALUE;
+        c[s.length()]=-1;
+        for(int i=s.length()-1;i>=0;i--){
+            for(int a=i,b=i;a>=0 && b<s.length() && s.charAt(a)==s.charAt(b);a--,b++) c[a]=Math.min(c[a],1+c[b+1]);
+            for(int a=i,b=i+1;a>=0 && b<s.length() && s.charAt(a)==s.charAt(b);a--,b++) c[a]=Math.min(c[a],1+c[b+1]);
         }
-        return cut[n];
+        return c[0];
     }
-};
-
+}

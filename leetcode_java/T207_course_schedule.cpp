9b4f1e632392b2c42cef6aaa00b70c8cf3e3d5a8
Dimitar9 <dimitar9909@gmail.com>
2015-07-09 16:39:12 -0400

    Update T207_course_schedule.cpp

--- leetcode_java/T207_course_schedule.cpp
+++ leetcode_java/T207_course_schedule.cpp
@@ -51,3 +51,61 @@ bool DFS(vector<unordered_set<int>> &matrix, unordered_set<int> &visited, int b,
     visited.erase(b);
     return false;
 }
+
+
+
+//3// OO solution
+public class Solution {
+    static class Course {
+        private boolean vis;
+        private boolean done;
+        private ArrayList<Course> pre = new ArrayList<Course>();
+
+        void addPre(Course preCourse) {
+            pre.add(preCourse);
+        }
+
+        boolean isCyclic() {
+            if( done ) {
+                return false;
+            }
+            if( vis ) {
+                return true;
+            }
+            vis = true;
+
+            for(Course preCourse: pre ) {
+                if( preCourse.isCyclic() ) {
+                    return true;
+                }
+            }
+
+            vis = false;
+            done = true;
+            return false;
+        }
+    }
+
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Course clist[] = new Course[numCourses];
+
+        for(int i=0; i<numCourses; i++) {
+            clist[i] = new Course();
+        }
+
+        for(int[] couple: prerequisites ) {
+            Course c1 = clist[couple[0]];
+            Course c2 = clist[couple[1]];
+            c1.addPre(c2);
+        }
+
+        for(int i=0; i<numCourses; i++) {
+            if( clist[i].isCyclic() ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

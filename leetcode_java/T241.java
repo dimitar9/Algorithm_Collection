756131e71696ad50be041582344d32923b1135a3
Dimitar9 <dimitar9909@gmail.com>
2015-08-04 22:39:08 -0400

    Create T241.java

---
+++ leetcode_java/T241.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> ret = new LinkedList<Integer>();
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i) == '-' ||
+                input.charAt(i) == '*' ||
+                input.charAt(i) == '+' ) {
+                String part1 = input.substring(0, i);
+                String part2 = input.substring(i+1);
+                List<Integer> part1Ret = diffWaysToCompute(part1);
+                List<Integer> part2Ret = diffWaysToCompute(part2);
+                for (Integer p1 :   part1Ret) {
+                    for (Integer p2 :   part2Ret) {
+                        int c = 0;
+                        switch (input.charAt(i)) {
+                            case '+': c = p1+p2;
+                                break;
+                            case '-': c = p1-p2;
+                                break;
+                            case '*': c = p1*p2;
+                                break;
+                        }
+                        ret.add(c);
+                    }
+                }
+            }
+        }
+        if (ret.size() == 0) {
+            ret.add(Integer.valueOf(input));
+        }
+        return ret;
+    }
+}
+

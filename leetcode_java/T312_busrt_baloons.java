7e852a37dc39efa4aeec0e08641e077b02aab295
Dimitar9 <dimitar9909@gmail.com>
2016-02-27 00:23:32 -0500

    Create T312_busrt_baloons.java

---
+++ leetcode_java/T312_busrt_baloons.java
@@ -0,0 +1,24 @@
+ public class Solution {
+public int maxCoins(int[] iNums) {
+    int[] nums = new int[iNums.length + 2];
+    int n = 1;
+    for (int x : iNums) if (x > 0) nums[n++] = x;
+    nums[0] = nums[n++] = 1;
+
+
+    int[][] memo = new int[n][n];
+    return burst(memo, nums, 0, n - 1);
+}
+
+public int burst(int[][] memo, int[] nums, int left, int right) {
+    if (left + 1 == right) return 0;
+    if (memo[left][right] > 0) return memo[left][right];
+    int ans = 0;
+    for (int i = left + 1; i < right; ++i)
+        ans = Math.max(ans, nums[left] * nums[i] * nums[right] 
+        + burst(memo, nums, left, i) + burst(memo, nums, i, right));
+    memo[left][right] = ans;
+    return ans;
+}
+// 12 ms
+}

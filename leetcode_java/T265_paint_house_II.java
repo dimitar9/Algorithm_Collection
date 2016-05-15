f8a37eb13e56972b9a201d565b4b621a7fa1f260
Dimitar9 <dimitar9909@gmail.com>
2016-05-15 16:52:16 -0400

    Create T265_paint_house_II.java

---
+++ leetcode_java/T265_paint_house_II.java
@@ -0,0 +1,45 @@
+public class Solution {
+    /**
+     * @param costs n x k cost matrix
+     * @return an integer, the minimum cost to paint all houses
+     */
+    public int minCostII(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+         
+        int n = costs.length;
+        int k = costs[0].length;
+         
+        // dp[j] means the min cost for color j
+        int[] dp = new int[k];
+        int min1 = 0;
+        int min2 = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            int oldMin1 = min1;
+            int oldMin2 = min2;
+             
+            min1 = Integer.MAX_VALUE;
+            min2 = Integer.MAX_VALUE;
+             
+            for (int j = 0; j < k; j++) {
+                if (dp[j] != oldMin1 || oldMin1 == oldMin2) {
+                    dp[j] = oldMin1 + costs[i][j];
+                } else {
+                    dp[j] = oldMin2 + costs[i][j];
+                }
+                 
+                if (min1 <= dp[j]) {
+                    min2 = Math.min(min2, dp[j]);
+                } else {
+                    min2 = min1;
+                    min1 = dp[j];
+                }
+            }
+             
+        }
+         
+        return min1;
+    }
+}
